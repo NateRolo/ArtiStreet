681af7b1330f7cb842abbafe14151c29d7810b1d
Nathan Oloresisimo <olor.nate@gmail.com>
2024-11-05 21:39:43 -0800

    added code to write to user collection in Firestore database upon login

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -1,20 +1,30 @@
-// Submit button
-document.getElementById('submit').addEventListener("click", function (event) {
-   event.preventDefault();
 
-   // Inputs
+ // Submit button 
+ document.getElementById('submit').addEventListener("click", function (event) {
+   event.preventDefault();
+ 
+   // Get form input values
    const email = document.getElementById('email').value;
    const password = document.getElementById('password').value;
-
-   // Sign up the user
+ 
+   // Sign up 
    firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then((userCredential) => {
-         // Successfully signed up
-         const user = userCredential.user;
-         window.location.href = "Landing.html";
-      })
-      .catch((error) => {
-         const errorMessage = error.message;
-         alert(errorMessage);
-      });
-});
+     .then((userCredential) => {
+       const user = userCredential.user;
+ 
+       // Write user data to Firestore in the "users" collection
+       return db.collection("users").doc(user.uid).set({
+         userID: user.uid,
+         email: user.email,
+         });
+     })
+     .then(() => {
+       // Redirect to the landing page
+       window.location.href = "Landing.html";
+     })
+     .catch((error) => {
+       const errorMessage = error.message;
+       alert(errorMessage);
+     });
+ });
+ 
