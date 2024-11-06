fedddcce60ae3b7a440652895ecd2be6a61eb661
Nathan Oloresisimo <olor.nate@gmail.com>
2024-11-05 22:12:40 -0800

    added button to send user to login page if they are already signed up

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -1,30 +1,41 @@
-
- // Submit button 
- document.getElementById('submit').addEventListener("click", function (event) {
-   event.preventDefault();
- 
-   // Get form input values
-   const email = document.getElementById('email').value;
-   const password = document.getElementById('password').value;
- 
-   // Sign up 
-   firebase.auth().createUserWithEmailAndPassword(email, password)
-     .then((userCredential) => {
-       const user = userCredential.user;
- 
-       // Write user data to Firestore in the "users" collection
-       return db.collection("users").doc(user.uid).set({
-         userID: user.uid,
-         email: user.email,
-         });
-     })
-     .then(() => {
-       // Redirect to the landing page
-       window.location.href = "Landing.html";
-     })
-     .catch((error) => {
-       const errorMessage = error.message;
-       alert(errorMessage);
-     });
+document.getElementById('login-redirect').addEventListener("click", function() {
+   window.location.href = "login.html"; // send to login page
  });
  
+document.getElementById('submit').addEventListener("click", function (event) {
+  event.preventDefault();
+
+
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const username = document.getElementById('username').value;
+  const userHandle = document.getElementById('userHandle').value;
+
+  // input validation
+  if (!email || !password || !username || !userHandle) {
+    alert("Please fill out all fields.");
+    return;
+  }
+
+  // Sign up 
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+
+      
+      return db.collection("users").doc(user.uid).set({
+        userID: user.uid,
+        email: user.email,
+        username: username,
+        userHandle: userHandle,
+      });
+    })
+    .then(() => {
+      // send to landing page
+      window.location.href = "Landing.html";
+    })
+    .catch((error) => {
+      const errorMessage = error.message;
+      alert(errorMessage);
+    });
+});
