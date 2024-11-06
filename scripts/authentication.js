78bcb4ca03c3c7bb5267d160582114666674ea3f
Jacob <jfonseca10@my.bcit.ca>
2024-11-05 19:55:11 -0800

    attempted debug on authentication.js

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -15,15 +15,12 @@ var uiConfig = {
         // Before this works, you must enable "Firestore" from the firebase console.
         // The Firestore rules must allow the user to write. 
         //------------------------------------------------------------------------------------------
-        var user = authResult.user;                            // get the user object from the Firebase authentication database
+        var user = authResult.user;
+        console.log("User Object: ", user);                    // get the user object from the Firebase authentication database
         if (authResult.additionalUserInfo.isNewUser) {         //if new user
             db.collection("users").doc(user.uid).set({         //write to firestore. We are using the UID for the ID in users collection
                    name: user.displayName,
-                   email: user.email,
-                   username: user.username,
-                   handle: user.handle,
-                   country: user.country,
-                   city: user.city
+                   email: user.email
             }).then(function () {
                    console.log("New user added to firestore");
                    window.location.assign("Landing.html");       //re-direct to main.html after signup
