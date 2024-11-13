7b100e0c2703edf7e356ac4889bd2f70fea57fa3
Tommy <timmythenintendo@gmail.com>
2024-11-13 02:32:09 -0800

    this is painful copy the each hike demo but it's still not working

--- public/js/content_view.js
+++ public/js/content_view.js
@@ -2,78 +2,61 @@
 
 async function displayUserInfo() {
     firebase.auth().onAuthStateChanged(user => {
-
-        console.log(user);
         if (!user) {
             console.log("You need to be signed in to see your posts.");
+            return;
         }
 
-        //go to the correct user document by referencing to the user uid
-        currentUser = db.collection("users").doc(user.uid);
-        //get the document for current user.
-        currentUser.get()
-            .then(userDoc => {
-                //get the data fields of the user
-                let userName = userDoc.data().username;
-                let userHandle = userDoc.data().userHandle;
-                let userBio = userDoc.data().userBio;
+        // Reference the current user's document by UID
+        let currentUser = db.collection("users").doc(user.uid);
 
-                //if the data fields are not empty, then write them in to the form.
-                if (userName != null) {
-                    document.getElementById("user-name").innerHTML = userName;
-                }
-                if (userHandle != null) {
-                    document.getElementById("user-handle").innerHTML = "@" + userHandle;
-                }
-                if (userBio != null) {
-                    document.getElementById("user-bio").innerHTML = userBio;
-                }
-            })
-    })
+        currentUser.get().then(userDoc => {
+            let userName = userDoc.data().username;
+            let userHandle = userDoc.data().userHandle;
+            let userBio = userDoc.data().userBio;
 
-
+            if (userName) document.getElementById("user-name").innerText = userName;
+            if (userHandle) document.getElementById("user-handle").innerText = "@" + userHandle;
+            if (userBio) document.getElementById("user-bio").innerText = userBio;
+        }).catch(error => {
+            console.error("Error fetching user data:", error);
+        });
+    });
 }
+
 displayUserInfo();
 
-document.querySelector("#nav-profile-tab").addEventListener("click", async () => {
-    db.collection("users").doc(user.uid).get()
-        .then(userDoc => {
-            var likes = userDoc.data.likes;
-            console.log(likes);
-            let newPostTemplate = document.getElementById("post-template");
+function displayPostInfo() {
+    let params = new URL(window.location.href); // Get URL of the search bar
+    let ID = params.searchParams.get("docID"); // Get value for key "docID"
+    if (!ID) {
+        console.error("No post ID found in URL.");
+        return;
+    }
+    console.log("Post ID:", ID);
 
-            likes.forEach(thisPostID);
-            db.collection("hikes").doc(thisPostId).get().then(doc => {
-                const title = doc.data().title;
-                const location = doc.data().street.concat(", " + doc.data().city);
-                const time = doc.data().time;
-                const imgURL = doc.data().image_URL;
-                const userName = doc.data().user.username;
-                const docID = doc.id;
+    // Fetch the post document
+    db.collection("posts")
+        .doc(ID)
+        .get()
+        .then(doc => {
+            if (!doc.exists) {
+                console.error("Post not found");
+                return;
+            }
+            const thisPost = doc.data();
+            const PostCode = thisPost.code;
+            const PostName = thisPost.name;
+            
+            // Populate title and image
+            document.getElementById("title").innerHTML = PostName;
+            let imgEvent = document.querySelector(".post-picture");
+            imgEvent.src = "./images/" + PostCode + ".jpg";
+        }).catch(error => {
+            console.error("Error fetching post data:", error);
+        });
 
-                let newPost = newPostTemplate.content.cloneNode(true);
-                const postPictureElement = newpost.querySelector('.post-picture');
-                if (postPictureElement && imgURL) {
-                    postPictureElement.src = imgURL;
-                }
+}
 
-                newpost.querySelector('.post-user').innerHTML = userName;
-                newpost.querySelector('.post-location').innerHTML = location;
-                newpost.querySelector('.post-title').innerHTML = title;
-                newpost.querySelector('.post-like').id = 'save-' + docID;
-                newpost.querySelector('.post-like').onclick = () => saveLike(docID);
-                
-                if (time) {
-                    const timeAgoText = timeAgo(time.toDate());
-                    newpost.querySelector('.post-time').innerHTML = timeAgoText;
-                } else {
-                    newpost.querySelector('.post-time').innerHTML = "Unknown time";
-                }
-
-                document.getElementById(collection + "-go-here").appendChild(newpost);
-            })
-
-        })
-
-});
+displayPostInfo();
 
