5a94269d7f469ea06eb0a936712608747bf7a804
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2024-11-12 18:51:50 -0800

    working on adding function so clicking "saved" displays only liked posts

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,7 +2,7 @@
 
 async function displayUserInfo() {
     firebase.auth().onAuthStateChanged(user => {
-        
+
         console.log(user);
         if (!user) {
             console.log("You need to be signed in to see your posts.");
@@ -34,3 +34,46 @@ async function displayUserInfo() {
 
 }
 displayUserInfo();
+
+document.querySelector("#nav-profile-tab").addEventListener("click", async () => {
+    db.collection("users").doc(user.uid).get()
+        .then(userDoc => {
+            var likes = userDoc.data.likes;
+            console.log(likes);
+            let newPostTemplate = document.getElementById("post-template");
+
+            likes.forEach(thisPostID);
+            db.collection("hikes").doc(thisPostId).get().then(doc => {
+                const title = doc.data().title;
+                const location = doc.data().street.concat(", " + doc.data().city);
+                const time = doc.data().time;
+                const imgURL = doc.data().image_URL;
+                const userName = doc.data().user.username;
+                const docID = doc.id;
+
+                let newPost = newPostTemplate.content.cloneNode(true);
+                const postPictureElement = newpost.querySelector('.post-picture');
+                if (postPictureElement && imgURL) {
+                    postPictureElement.src = imgURL;
+                }
+
+                newpost.querySelector('.post-user').innerHTML = userName;
+                newpost.querySelector('.post-location').innerHTML = location;
+                newpost.querySelector('.post-title').innerHTML = title;
+                newpost.querySelector('.post-like').id = 'save-' + docID;
+                newpost.querySelector('.post-like').onclick = () => saveLike(docID);
+                
+                if (time) {
+                    const timeAgoText = timeAgo(time.toDate());
+                    newpost.querySelector('.post-time').innerHTML = timeAgoText;
+                } else {
+                    newpost.querySelector('.post-time').innerHTML = "Unknown time";
+                }
+
+                document.getElementById(collection + "-go-here").appendChild(newpost);
+            })
+
+        })
+
+});
+
